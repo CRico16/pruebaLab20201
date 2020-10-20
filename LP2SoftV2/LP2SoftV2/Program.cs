58f1ac0df62fc50cf22f605abbe72d102b3ac9a8
CRico <cesarrico16@gmail.com>
2020-10-19 19:07:23 -0500

    Se ha creado un proeycto en .NET

---
+++ LP2SoftV2/LP2SoftV2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP2SoftV2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Mi primer programa");
+            System.Console.Read();
+        }
+    }
+}
